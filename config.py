73a117430601778895c2ab4c67b4e16cbda08625
tejp1234 <always.growwithbro@gmail.com>
2025-10-16 23:26:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,17 +13,17 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = int(getenv("API_ID", "22141398"))
-API_HASH = getenv("API_HASH", "0c8f8bd171e05e42d6f6e5a6f4305389")
-BOT_TOKEN = getenv("BOT_TOKEN", "8269760529:AAGeGEyX024CysP2UT-NeJ0vBok3k387RI8")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "8104777494").split()))
+API_ID = int(getenv("API_ID", "26342348"))
+API_HASH = getenv("API_HASH", "aac8f8c983c97a08db79ece3eee2b9be")
+BOT_TOKEN = getenv("BOT_TOKEN", "8307279773:AAEOcFdbSoF0Eciwzdw9xMmF0c1-P39Zh78")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "5763227969").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://Ansh089:Ansh089@cluster0.y8tpouc.mongodb.net/?retryWrites=true&w=majority")
-LOG_GROUP = getenv("LOG_GROUP", "-1003003594014")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1003003594014"))
-FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
+LOG_GROUP = getenv("LOG_GROUP", "-1003108906018")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1003108906018"))
+FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "500"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
 WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
-AD_API = getenv("AD_API", "52b4a2cf4687d81e7d3f8f2b7bc2943f618e78cb")
+AD_API = getenv("AD_API", "aac8f8c983c97a08db79ece3eee2b9be")
 STRING = getenv("STRING", None)
 YT_COOKIES = getenv("YT_COOKIES", YTUB_COOKIES)
 DEFAULT_SESSION = getenv("DEFAUL_SESSION", None)  # added old method of invite link joining
